39ce1b7c24b4b3538905a70bb149f43cb63eafd2
Nguyễn Thái Bình <120584598+Thai-Binh727@users.noreply.github.com>
2024-12-28 13:41:45 +0700

    update test cho extension

---
+++ Server/server_test.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+urls = ['https://mail.google.com/',
+        'https://www.youtube.com/',
+        'https://web.facebook.com/',
+        'https://www.messenger.com/']
+
+@app.route("/check-url", methods=["POST"])
+def check_url():
+    data = request.get_json()
+    url_inp = data.get("url", "")
+    is_malicious = False
+    if url_inp in urls:
+        is_malicious = True
+
+    return jsonify({
+        "url": url_inp,
+        "isMalicious": is_malicious,
+    })
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0' , port = 5000)
